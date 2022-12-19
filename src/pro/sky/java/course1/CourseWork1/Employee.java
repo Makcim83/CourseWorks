496bb7ac13f27347a18075214c183d3b84dadf98
admin <mmd83@63.ru>
2022-12-19 23:44:30 +0400

    CourseWork1 Задача: написать программу, которая помогает автоматизировать процессы бухгалтерии и отдела кадров по учету сотрдников.
    Выполнена базовая сложность - написать основные методы.

---
+++ src/pro/sky/java/course1/CourseWork1/Employee.java
@@ -0,0 +1,97 @@
+package pro.sky.java.course1.CourseWork1;
+
+import java.util.Objects;
+
+public class Employee {
+    public static int id = 0;
+    private String lastName;
+    private String firstName;
+    private String patronymic;
+    private int departmentNumber;
+    private int salary;
+    private int idEmployee;
+
+    public Employee(String lastName, String patronymic, String firstName, int departmentNumber, int salary) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.patronymic = patronymic;
+        this.departmentNumber = departmentNumber;
+        this.salary = salary;
+        idEmployee = id;
+        id++;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getDepartmentNumber() {
+        return departmentNumber;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getIdEmployee() {
+        return idEmployee;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setDepartmentNumber(int departmentNumber) {
+        this.departmentNumber = departmentNumber;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String toStringFullName() {
+        return lastName + " " + firstName + " " + patronymic;
+    }
+
+    @Override
+    public String toString() {
+        return "Ф.И.О. " + toStringFullName() +
+                "\nНомер департамента " + departmentNumber +
+                "\nЗарплата " + salary +
+                "\nid " + idEmployee;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return departmentNumber == employee.departmentNumber
+                && salary == employee.salary
+                && idEmployee == employee.idEmployee
+                && firstName.equals(employee.firstName)
+                && patronymic.equals(employee.patronymic)
+                && lastName.equals(employee.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, patronymic, lastName, departmentNumber, salary, idEmployee);
+    }
+}
--- src/pro/sky/java/course1/CourseWork1/Main.java
+++ src/pro/sky/java/course1/CourseWork1/Main.java
@@ -1,7 +1,80 @@
 package pro.sky.java.course1.CourseWork1;
 
 public class Main {
+    public static Employee EmployeeBook[] = new Employee[10];
     public static void main(String[] args) {
 
+        EmployeeBook[0] = new Employee("Иванов", "Иван", "Иванович", 1, 30000);
+        EmployeeBook[1] = new Employee("Петров", "Иван", "Иванович", 2, 40000);
+        EmployeeBook[2] = new Employee("Сидоров", "Иван", "Иванович", 3, 50000);
+        EmployeeBook[3] = new Employee("Кузнецов", "Иван", "Иванович", 4, 60000);
+        EmployeeBook[4] = new Employee("Романов", "Иван", "Иванович", 5, 70000);
+
+        allEmployeeToPrint();
+        System.out.println("Сумма затрат на зарплату " + amountSalaryOfAllEmployeers());
+        System.out.println("Сотрудник с минимальной зарплатой " + minimalSalaryEmployee().toStringFullName());
+        System.out.println("Сотрудник с максимальной зарплатой " + maximumSalaryEmployee().toStringFullName());
+        System.out.println("Среднее значение зарплат " + amountSalaryOfAllEmployeers() / numEmployeers());
+
+    }
+
+    public static void allEmployeeToPrint() {
+        for (Employee w : EmployeeBook
+        ) {
+            if (w != null) {
+                System.out.println(w.toString());
+            }
+        }
+    }
+    public static int amountSalaryOfAllEmployeers() {
+        int sum = 0;
+        for (Employee w : EmployeeBook
+            ) {
+            if (w != null) {
+                sum+=w.getSalary();
+            }
+        }
+        return sum;
+    }
+    public static Employee minimalSalaryEmployee() {
+        int min=0;
+        int idEmployee=0;
+        for (int i = 0; i < EmployeeBook.length; i++) {
+            if(EmployeeBook[i] != null) {
+                min = EmployeeBook[i].getSalary();
+            }
+        }
+        for (int i = 0; i < EmployeeBook.length; i++) {
+            if(EmployeeBook[i] != null) {
+                if (EmployeeBook[i].getSalary() < min){
+                    min = EmployeeBook[i].getSalary();
+                    idEmployee = i;
+                }
+            }
+        }
+        return EmployeeBook[idEmployee];
+    }
+    public static Employee maximumSalaryEmployee() {
+        int max=0;
+        int idEmployee=0;
+        for (int i = 0; i < EmployeeBook.length; i++) {
+            if(EmployeeBook[i] != null) {
+                if (EmployeeBook[i].getSalary() > max){
+                    max = EmployeeBook[i].getSalary();
+                    idEmployee = i;
+                }
+            }
+        }
+        return EmployeeBook[idEmployee];
+    }
+    public static int numEmployeers() {
+        int num = 0;
+        for (Employee w : EmployeeBook
+        ) {
+            if (w != null) {
+                num++;
+            }
+        }
+        return num;
     }
 }
